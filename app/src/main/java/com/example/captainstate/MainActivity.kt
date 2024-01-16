09da5800887392f5dc944a9a0a9757564d063570
sidhant-bhat <bhat.sid2001@gmail.com>
2024-01-17 00:28:31 +0530

    adding files

---
+++ app/src/main/java/com/example/captainstate/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.captainstate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.captainstate.ui.theme.CaptainStateTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaptainStateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CaptainGame()
+
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+@Composable
+fun CaptainGame() {
+    val trasuresFound= remember { mutableDoubleStateOf(0.0)}
+    val direction = remember { mutableStateOf("North") }
+    val stormOrTreasure = remember { mutableStateOf(" ") }
+   Column {
+       Text(text = "Treasures Found: ${trasuresFound.value}")
+       Text(text = "Current  Direction: ${direction.value}")
+       Text(text = "StormOrTressure: ${stormOrTreasure.value}")
+       Button(onClick = { direction.value = "East"
+       if (Random.nextBoolean()){
+           trasuresFound.value+=1
+           stormOrTreasure.value="found a treassure"
+       }
+           else{
+               stormOrTreasure.value="Storm Ahead"
+       }
+
+       }) {
+           Text(text = "Sail East")
+       }
+       Button(onClick = { direction.value = "West"
+           if (Random.nextBoolean()){
+               trasuresFound.value+=1
+               stormOrTreasure.value="Found a Tressure"
+           }
+           else{
+               stormOrTreasure.value="Storm a head"
+           }
+
+       }) {
+           Text(text = "Sail West")
+       }
+       Button(onClick = { direction.value = "North"
+           if (Random.nextBoolean()){
+               trasuresFound.value+=1
+              stormOrTreasure.value="Found a trssure"
+           }
+           else{
+               stormOrTreasure.value="Storm ahead"
+           }
+
+       }) {
+           Text(text = "Sail North")
+       }
+       Button(onClick = { direction.value = "South"
+           if (Random.nextBoolean()){
+               trasuresFound.value+=1
+               stormOrTreasure.value="Found a treassure"
+           }
+           else{
+               stormOrTreasure.value="Storm ahead"
+           }
+
+       }) {
+           Text(text = "Sail South")
+       }
+
+   }
+    
+}
+
+
+
+
+
+
+
